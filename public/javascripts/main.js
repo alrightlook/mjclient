f206dfa9843bcb630996f1340114d404cce5ad9f
Alrightlook <alrightlook@gmail.com>
2018-03-01 01:37:44 +0800

    add background

--- public/javascripts/main.js
+++ public/javascripts/main.js
@@ -1,8 +1,8 @@
 var config = {
     type : Phaser.AUTO,
     parent : 'game',
-    width : 640,
-    height : 480,
+    width : 1334,
+    height : 750,
     scene: {
         preload : preload,
         create : create
@@ -11,10 +11,12 @@ var config = {
 
 function preload()
 {
+    this.load.image('bg', 'assets/background.jpg');
 }
 
 function create()
 {
+    this.add.image(1334 / 2, 750 / 2, 'bg');
 }
 
 var game = new Phaser.Game(config);
